0f1a4e3f933e4ea4526bf4daded8b5099664502d
Jainal Gosaliya <jainal09gosalia@gmail.com>
2020-03-13 15:41:00 +0530

    Update Evaluate.py

--- Evaluate.py
+++ Evaluate.py
@@ -12,192 +12,195 @@ io_yaml_abs_path = os.path.abspath('strivio/yaml_files/IO.yaml')
 class Evaluater:
 
     def evaluate(self):
-        io_yaml_parsed_dic = my_obj.parse_yaml(io_yaml_abs_path)
-        io_dic = my_obj.io_fetcher(io_yaml_parsed_dic)
-        language_dic = my_obj.parse_yaml(os.path.abspath("lang.yaml"))
-        language = language_dic["language"]
-        inputs = io_dic["inputs"]
-        outputs = io_dic["outputs"]
-        if language:
-            if str(language) in LANGUAGES:
-                to_check_extension = LANGUAGES[language]
-                if language == "python3":
-                    file_extension = pathlib.Path(
-                        os.path.abspath(
-                            "program.py"
-                        )
-                    ).suffix
-                    if file_extension == to_check_extension:
-                        case = 1
-                        for inp, outp in zip(inputs, outputs):
-                            data, temp = os.pipe()
-                            os.write(temp, bytes(str(inp)+"\n", "utf-8"))
-                            os.close(temp)
-                            output = subprocess.check_output(
-                                [
-                                    "python3",
-                                 os.path.abspath(
-                                     "program.py"
-                                 )
-                                ],
-                                stdin=data
-                            )
-                            output = output.decode("utf-8")
-                            output = output.strip('\n')
-                            if output == str(outp):
-                                print("Test Case " + str(case) + ": Passed")
-                                case = case + 1
-                            else:
-                                raise Exception(
-                                    "Test Case " + str(case) + ": Failed"
-                                )
+          a = os.walk(os.path.abspath('strivio'))
+          for b in a:
+            print(b)
+#         io_yaml_parsed_dic = my_obj.parse_yaml(io_yaml_abs_path)
+#         io_dic = my_obj.io_fetcher(io_yaml_parsed_dic)
+#         language_dic = my_obj.parse_yaml(os.path.abspath("lang.yaml"))
+#         language = language_dic["language"]
+#         inputs = io_dic["inputs"]
+#         outputs = io_dic["outputs"]
+#         if language:
+#             if str(language) in LANGUAGES:
+#                 to_check_extension = LANGUAGES[language]
+#                 if language == "python3":
+#                     file_extension = pathlib.Path(
+#                         os.path.abspath(
+#                             "program.py"
+#                         )
+#                     ).suffix
+#                     if file_extension == to_check_extension:
+#                         case = 1
+#                         for inp, outp in zip(inputs, outputs):
+#                             data, temp = os.pipe()
+#                             os.write(temp, bytes(str(inp)+"\n", "utf-8"))
+#                             os.close(temp)
+#                             output = subprocess.check_output(
+#                                 [
+#                                     "python3",
+#                                  os.path.abspath(
+#                                      "program.py"
+#                                  )
+#                                 ],
+#                                 stdin=data
+#                             )
+#                             output = output.decode("utf-8")
+#                             output = output.strip('\n')
+#                             if output == str(outp):
+#                                 print("Test Case " + str(case) + ": Passed")
+#                                 case = case + 1
+#                             else:
+#                                 raise Exception(
+#                                     "Test Case " + str(case) + ": Failed"
+#                                 )
 
-                    else:
-                        raise Exception(
-                            "language in lang.yaml and program file doesnt match"
-                        )
-                elif language == "cpp":
-                    file_extension = pathlib.Path(
-                        os.path.abspath(
-                            "program.cpp"
-                        )
-                    ).suffix
-                    if file_extension == to_check_extension:
-                        subprocess.call(
-                            [
-                                "g++",
-                                "-o",
-                                os.path.abspath(
-                                    "program"
-                                ),
-                                os.path.abspath(
-                                    "program.cpp"
-                                )
-                            ]
-                        )
-                        case = 1
-                        for inp, outp in zip(inputs, outputs):
-                            data, temp = os.pipe()
-                            os.write(temp, bytes(str(inp)+"\n", "utf-8"))
-                            os.close(temp)
-                            output = subprocess.check_output(
-                                [
-                                    os.path.abspath(
-                                        "program"
-                                    )
-                                ],
-                                stdin=data
-                            )
-                            output = output.decode("utf-8")
-                            output = output.strip('\n')
-                            if output == str(outp):
-                                print("Test Case " + str(case) + ": Passed")
-                                case = case + 1
-                            else:
-                                raise Exception(
-                                    "Test Case " + str(case) + ": Failed"
-                                )
+#                     else:
+#                         raise Exception(
+#                             "language in lang.yaml and program file doesnt match"
+#                         )
+#                 elif language == "cpp":
+#                     file_extension = pathlib.Path(
+#                         os.path.abspath(
+#                             "program.cpp"
+#                         )
+#                     ).suffix
+#                     if file_extension == to_check_extension:
+#                         subprocess.call(
+#                             [
+#                                 "g++",
+#                                 "-o",
+#                                 os.path.abspath(
+#                                     "program"
+#                                 ),
+#                                 os.path.abspath(
+#                                     "program.cpp"
+#                                 )
+#                             ]
+#                         )
+#                         case = 1
+#                         for inp, outp in zip(inputs, outputs):
+#                             data, temp = os.pipe()
+#                             os.write(temp, bytes(str(inp)+"\n", "utf-8"))
+#                             os.close(temp)
+#                             output = subprocess.check_output(
+#                                 [
+#                                     os.path.abspath(
+#                                         "program"
+#                                     )
+#                                 ],
+#                                 stdin=data
+#                             )
+#                             output = output.decode("utf-8")
+#                             output = output.strip('\n')
+#                             if output == str(outp):
+#                                 print("Test Case " + str(case) + ": Passed")
+#                                 case = case + 1
+#                             else:
+#                                 raise Exception(
+#                                     "Test Case " + str(case) + ": Failed"
+#                                 )
 
-                    else:
-                        raise Exception(
-                            "language in lang.yaml and program file doesnt match"
-                        )
-                elif language == "c":
-                    file_extension = pathlib.Path(
-                        os.path.abspath(
-                            "program.c"
-                        )
-                    ).suffix
-                    if file_extension == to_check_extension:
-                        subprocess.call(
-                            [
-                                "gcc",
-                                "-o",
-                                os.path.abspath(
-                                    "program"
-                                ),
-                                os.path.abspath(
-                                    "program.c"
-                                )
-                            ]
-                        )
-                        case = 1
-                        for inp, outp in zip(inputs, outputs):
-                            data, temp = os.pipe()
-                            os.write(temp, bytes(str(inp)+"\n", "utf-8"))
-                            os.close(temp)
-                            output = subprocess.check_output(
-                                [
-                                    os.path.abspath("program")
-                                ],
-                                stdin=data
-                            )
-                            output = output.decode("utf-8")
-                            output = output.strip('\n')
-                            if output == str(outp):
-                                print("Test Case " + str(case) + ": Passed")
-                                case = case + 1
-                            else:
-                                raise Exception(
-                                    "Test Case " + str(case) + ": Failed"
-                                )
+#                     else:
+#                         raise Exception(
+#                             "language in lang.yaml and program file doesnt match"
+#                         )
+#                 elif language == "c":
+#                     file_extension = pathlib.Path(
+#                         os.path.abspath(
+#                             "program.c"
+#                         )
+#                     ).suffix
+#                     if file_extension == to_check_extension:
+#                         subprocess.call(
+#                             [
+#                                 "gcc",
+#                                 "-o",
+#                                 os.path.abspath(
+#                                     "program"
+#                                 ),
+#                                 os.path.abspath(
+#                                     "program.c"
+#                                 )
+#                             ]
+#                         )
+#                         case = 1
+#                         for inp, outp in zip(inputs, outputs):
+#                             data, temp = os.pipe()
+#                             os.write(temp, bytes(str(inp)+"\n", "utf-8"))
+#                             os.close(temp)
+#                             output = subprocess.check_output(
+#                                 [
+#                                     os.path.abspath("program")
+#                                 ],
+#                                 stdin=data
+#                             )
+#                             output = output.decode("utf-8")
+#                             output = output.strip('\n')
+#                             if output == str(outp):
+#                                 print("Test Case " + str(case) + ": Passed")
+#                                 case = case + 1
+#                             else:
+#                                 raise Exception(
+#                                     "Test Case " + str(case) + ": Failed"
+#                                 )
 
-                    else:
-                        raise Exception(
-                            "language in lang.yaml and program file doesnt match"
-                        )
-                elif language == "java":
-                    file_extension = pathlib.Path(
-                        os.path.abspath(
-                            "program.java"
-                        )
-                    ).suffix
-                    if file_extension == to_check_extension:
-                        subprocess.call(
-                            [
-                                "javac",
-                                 os.path.abspath("Program.java")
-                            ]
-                        )
-                        case = 0
-                        for inp, outp in zip(inputs, outputs):
-                            data, temp = os.pipe()
-                            os.write(temp, bytes(str(inp)+"\n", "utf-8"))
-                            os.close(temp)
-                            output = subprocess.check_output(
-                                [
-                                    "java",
-                                    "Program"
-                                ],
-                                stdin=data
-                            )
-                            output = output.decode("utf-8")
-                            output = output.strip('\n')
-                            if output == str(outp):
-                                print("Test Case " + str(case) + ": Passed")
-                                case = case + 1
-                            else:
-                                raise Exception(
-                                    "Test Case " + str(case) + ": Failed"
-                                )
+#                     else:
+#                         raise Exception(
+#                             "language in lang.yaml and program file doesnt match"
+#                         )
+#                 elif language == "java":
+#                     file_extension = pathlib.Path(
+#                         os.path.abspath(
+#                             "program.java"
+#                         )
+#                     ).suffix
+#                     if file_extension == to_check_extension:
+#                         subprocess.call(
+#                             [
+#                                 "javac",
+#                                  os.path.abspath("Program.java")
+#                             ]
+#                         )
+#                         case = 0
+#                         for inp, outp in zip(inputs, outputs):
+#                             data, temp = os.pipe()
+#                             os.write(temp, bytes(str(inp)+"\n", "utf-8"))
+#                             os.close(temp)
+#                             output = subprocess.check_output(
+#                                 [
+#                                     "java",
+#                                     "Program"
+#                                 ],
+#                                 stdin=data
+#                             )
+#                             output = output.decode("utf-8")
+#                             output = output.strip('\n')
+#                             if output == str(outp):
+#                                 print("Test Case " + str(case) + ": Passed")
+#                                 case = case + 1
+#                             else:
+#                                 raise Exception(
+#                                     "Test Case " + str(case) + ": Failed"
+#                                 )
 
-                    else:
-                        raise Exception(
-                            "language in lang.yaml and program file doesnt match"
-                        )
-                else:
-                    raise Exception(
-                        "Not supported language in lang.yaml"
-                    )
+#                     else:
+#                         raise Exception(
+#                             "language in lang.yaml and program file doesnt match"
+#                         )
+#                 else:
+#                     raise Exception(
+#                         "Not supported language in lang.yaml"
+#                     )
 
-            else:
-                raise Exception(
-                    "Not supported language in lang.yaml"
-                )
-        else:
-            raise Exception(
-                "lang.yaml not found"
-            )
+#             else:
+#                 raise Exception(
+#                     "Not supported language in lang.yaml"
+#                 )
+#         else:
+#             raise Exception(
+#                 "lang.yaml not found"
+#             )
 if __name__ == '__main__':
     eval_obj = Evaluater()
     eval_obj.evaluate()
