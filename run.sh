a766912a5e19f3c08b4a2390b0dbafbd19221bf9
Jainal Gosaliya <jainal09gosalia@gmail.com>
2020-03-13 18:21:48 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+echo $INPUT_OATH
+echo $INPUT_OATH >> /strivio/yaml_files/dropbox_uploader.conf
+chmod +x /strivio/yaml_files/dropbox_uploader.sh
+/strivio/yaml_files/dropbox_uploader.sh -f \
+  /strivio/yaml_files/dropbox_uploader.conf download IO.yaml ./yaml_files/IO.yaml
+python3 /strivio/Evaluate.py
