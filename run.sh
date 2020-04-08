d9f3de229d7e150629da2df4632343b5039a8cae
Jainal Gosaliya <jainal09gosalia@gmail.com>
2020-04-09 00:42:17 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,6 @@ chmod +x /strivio/yaml_files/dropbox_uploader.sh
 /strivio/yaml_files/dropbox_uploader.sh -f \
   /strivio/yaml_files/dropbox_uploader.conf download IO.yaml /strivio/yaml_files/IO.yaml
 ls
+cat /strivio/yaml_files/IO.yaml
 python3 --version
 python3 /strivio/Evaluate.py
