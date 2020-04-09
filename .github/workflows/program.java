e36803c3e8e7f6bd31db272412d8257aa5c7668b
Jainal Gosaliya <jainal09gosalia@gmail.com>
2020-04-09 06:04:48 +0530

    Create program.java

---
+++ .github/workflows/program.java
@@ -0,0 +1,13 @@
+import java.util.*;  
+
+class Main
+{
+  public static void main(String args[])
+  {
+     String num;
+   
+     Scanner in = new Scanner(System.in);
+     num = in.nextLine();
+     System.out.println(num+" ghi");
+  }
+}
