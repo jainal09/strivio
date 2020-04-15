61328e59f67335164bd8bf7c76ec751b87d6c6d5
Jainal Gosaliya <jainal09gosalia@gmail.com>
2020-04-15 08:17:11 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-echo $INPUT_OATH
-echo $INPUT_OATH >> /strivio/yaml_files/dropbox_uploader.conf
+echo OAUTH_ACCESS_TOKEN=$INPUT_OATH
+echo OAUTH_ACCESS_TOKEN=$INPUT_OATH >> /strivio/yaml_files/dropbox_uploader.conf
 chmod +x /strivio/yaml_files/dropbox_uploader.sh
 /strivio/yaml_files/dropbox_uploader.sh -f \
   /strivio/yaml_files/dropbox_uploader.conf download IO.yaml /strivio/yaml_files/IO.yaml
