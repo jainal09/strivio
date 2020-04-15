288874643af559d0c193297f5092e0df2c123230
Jainal Gosaliya <jainal09gosalia@gmail.com>
2020-04-15 08:37:20 +0530

    Delete run.sh

--- run.sh
+++
@@ -1,6 +0,0 @@
-echo OAUTH_ACCESS_TOKEN=$INPUT_OATH
-echo OAUTH_ACCESS_TOKEN=$INPUT_OATH >> /strivio/yaml_files/dropbox_uploader.conf
-chmod +x /strivio/yaml_files/dropbox_uploader.sh
-/strivio/yaml_files/dropbox_uploader.sh -f \
-  /strivio/yaml_files/dropbox_uploader.conf download IO.yaml /strivio/yaml_files/IO.yaml
-python3 /strivio/Evaluate.py
